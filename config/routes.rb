a2a21e11d26e693de70fd41fe1e116063d9a40e5
Genki Kurihara <spiral809@gmail.com>
2019-09-05 14:07:02 +0900

    revised controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
-  get 'reviews/create'
-  get 'reviews/new'
-  get 'restaurants/create'
-  get 'restaurants/new'
-  get 'restaurants/index'
-  get 'restaurants/show'
-  get 'restaurant/create'
-  get 'restaurant/new'
-  get 'restaurant/index'
-  get 'restaurant/show'
-  resources :restaurants, only: [ :create, :index, :show, :new, :edit] do
-    resources :reviews, only: [ :new, :create]
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create, :new]
   end
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
